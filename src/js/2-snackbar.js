06a58641f5624f7838ed90800b529daf88f937d0
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-03-16 15:04:13 +0100

    fix js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,8 +6,26 @@ const formRef = document.querySelector('form');
 formRef.addEventListener('submit', creatMessage);
 
 function creatMessage(event) {
-    event.preventDefault();
-    createPromise(formRef.elements.delay.value);
+  event.preventDefault();
+  const promise = createPromise(formRef.elements.delay.value);
+  promise
+    .then(delay => {
+    iziToast.show({
+      message: `✅ Fulfilled promise in ${delay}ms`,
+      color: 'green',
+      position: 'topRight',
+      timeout: 5000,
+    })
+    })
+    .catch(delay => {
+    iziToast.show({
+      message: `❌ Rejected promise in ${delay}ms`,
+      color: 'red',
+      position: 'topRight',
+      timeout: 5000,
+    })
+    })
+
     formRef.elements.delay.value = "";
 
 }
@@ -16,23 +34,9 @@ function createPromise(delay) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (formRef.elements.state.value === 'fulfilled') {
-          resolve(
-            iziToast.show({
-              message: `✅ Fulfilled promise in ${delay}ms`,
-              color: 'green',
-              position: 'topRight',
-              timeout: 5000,              
-          })
-        )
-      } else {
-          reject(
-            iziToast.show({
-              message: `❌ Rejected promise in ${delay}ms`,
-              color: 'red',
-              position: 'topRight',
-              timeout: 5000,              
-          })
-        )
+        resolve(delay)
+      }else if (formRef.elements.state.value === 'rejected') {
+        reject(delay)
       }
     }, delay);
   });
